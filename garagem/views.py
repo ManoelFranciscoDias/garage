06c2f1d792f233b5ebfd2d7288dd185adc898f12
ManoelFranciscoDias <manoel2007dias@gmail.com>
2025-04-25 14:32:51 -0300

    teste commit

--- garagem/views.py
+++ garagem/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.permissions import IsAuthenticated
 
 from garagem.models import Categoria, Editora, Livro, Autor
 from garagem.serializers import CategoriaSerializer, EditoraSerializer, LivroSerializer, AutorSerializer, LivroDetailSerializer, LivroListSerializer
@@ -7,6 +7,7 @@ from garagem.serializers import CategoriaSerializer, EditoraSerializer, LivroSer
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+    permission_classes = [IsAuthenticated]
 
 
 
